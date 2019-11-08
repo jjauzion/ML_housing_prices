ffd854d49b62531b2ec1d5daf608b8072282bf9f
Jeremy JAUZION <jjauzion@e3r5p22.42.fr>
2019-11-08 10:32:03 +0100

    filter out feature with 0 nan

--- nan_filter.py
+++ nan_filter.py
@@ -12,11 +12,12 @@ data_conf = toolbox.DataConf()
 try:
     data_conf.import_from_json(args.file)
 except Exception as err:
-    print(f"Error: can't read file '{Path(args.file)}' because: {err.__class__.__name__}:{err}")
+    print(f"Error: can't read json configuration file '{Path(args.file)}' because: {err.__class__.__name__}:{err}")
     exit(0)
 
 df = pd.read_csv(data_conf.raw_data_file)
 null = df.isnull().sum()
 percentage = null / df.isnull().count()
 synthesis = pd.concat((null, percentage), axis=1, keys=("Total", "Percentage")).sort_values(ascending=False, by="Total")
+synthesis = synthesis[synthesis["Total"] != 0]
 print(synthesis)
